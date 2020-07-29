0f4d4d3f3246a3329880823c2643fa5bbd26c6bb
anil gola <anil.gola@nagarro.com>
2020-07-29 15:24:20 +0530

    first commit

---
+++ src/main/java/org/anil/design/model/CreditCard.java
@@ -0,0 +1,31 @@
+package com.example.design.model;
+
+import java.util.Date;
+
+public class CreditCard {
+	
+	String cardHolderName;
+	Date expiryDate;
+	int cvv;
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public int getCvv() {
+		return cvv;
+	}
+	public void setCvv(int cvv) {
+		this.cvv = cvv;
+	}
+	
+	
+
+}
